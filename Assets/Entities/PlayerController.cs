db65360a3ea3d71ed6cad408550950e40fb4d205
Manuel Reinfurt <manuel.reinfurt@outlook.com>
2016-06-01 15:48:34 +0200

    Joysticks are now supported, fixes #15

--- Assets/Entities/PlayerController.cs
+++ Assets/Entities/PlayerController.cs
@@ -65,31 +65,11 @@ namespace Entities
 
         private void HandleInput()
         {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                this.movement.x = 1;
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                this.movement.x = -1;
-            }
-            else
-            {
-                this.movement.x = 0;
-            }
-
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                this.movement.y = 1;
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                this.movement.y = -1;
-            }
-            else
-            {
-                this.movement.y = 0;
-            }
+            this.movement.x = Input.GetAxis(Game.Definitions.Player.HorizontalMovement) +
+                              Input.GetAxis(Game.Definitions.Player.JoystickHorizontalMovement);
+            this.movement.y = Input.GetAxis(Game.Definitions.Player.VerticalMovement) +
+                              Input.GetAxis(Game.Definitions.Player.JoystickVerticalMovement);
+            this.movement.Normalize();
         }
 
         private void HandleMovement()
---
+++ Assets/Game/Definitions.cs
@@ -0,0 +1,18 @@
+﻿namespace Game
+{
+    namespace Definitions
+    {
+        public class Player
+        {
+            #region Public
+
+            public static string HorizontalMovement = "Horizontal";
+            public static string VerticalMovement = "Vertical";
+
+            public static string JoystickHorizontalMovement = "HorizontalJoystick";
+            public static string JoystickVerticalMovement = "VerticalJoystick";
+
+            #endregion
+        }
+    }
+}
