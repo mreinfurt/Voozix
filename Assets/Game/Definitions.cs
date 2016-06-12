fa46ee212030619da5fed1f5ad0ef08ea0b565ee
Manuel Reinfurt <manuel.reinfurt@outlook.com>
2016-06-12 12:00:14 +0200

    Added different LocalizationKeys for Desktop/Console/Touch devices

--- Assets/Game/Definitions.cs
+++ Assets/Game/Definitions.cs
@@ -14,5 +14,35 @@
 
             #endregion
         }
+
+        namespace LocalizationKeys
+        {
+            /// <summary>
+            /// Keyboard
+            /// </summary>
+            public class Desktop
+            {
+                public static string TryAgain = "Press <color=\"#fffc19\">R</color> to try again";
+                public static string Score = "Score";
+            }
+
+            /// <summary>
+            /// Controller
+            /// </summary>
+            public class Console
+            {
+                public static string TryAgain = "Press <color=\"#fffc19\">X</color> to try again";
+                public static string Score = "Score";
+            }
+
+            /// <summary>
+            /// Touch-Device
+            /// </summary>
+            public class Touch
+            {
+                public static string TryAgain = "<color=\"#fffc19\">Tap</color> to try again";
+                public static string Score = "Score";
+            }
+        }
     }
 }
