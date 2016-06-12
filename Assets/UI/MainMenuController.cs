d8667a91fdb144e808232b6b1d2b20668ac9c601
Manuel Reinfurt <manuel.reinfurt@outlook.com>
2016-06-12 13:29:39 +0200

    First iteration of the MainMenu scene

---
+++ Assets/UI/MainMenuController.cs
@@ -0,0 +1,55 @@
+﻿#region Namespaces
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#endregion
+
+public class MainMenuController : MonoBehaviour
+{
+    #region Fields
+
+    public Button PlayCampaignButton;
+
+    public Button PlaySurvivalButton;
+    public Button Quit;
+
+    #endregion
+
+    #region Methods
+
+    void Start()
+    {
+        this.PlaySurvivalButton.onClick.AddListener(HandlePlaySurvivalButtonClick);
+        this.PlayCampaignButton.onClick.AddListener(HandlePlayCampaignButtonClick);
+        this.Quit.onClick.AddListener(HandleQuitButtonClick);
+    }
+
+    private void HandleQuitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    private void HandlePlayCampaignButtonClick()
+    {
+    }
+
+    private void HandlePlaySurvivalButtonClick()
+    {
+        if (NiceSceneTransition.instance != null)
+        {
+            NiceSceneTransition.instance.LoadScene("Survival");
+        }
+        else
+        {
+            SceneManager.LoadScene("Survival", LoadSceneMode.Single);
+        }
+    }
+
+    void Update()
+    {
+    }
+
+    #endregion
+}
